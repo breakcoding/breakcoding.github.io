ae73b362775c65269bed05167020ea90ac9e2141
DESKTOP-SR4BQIA\ernes <asesor22@apiinmobiliaria.com.mx>
2017-11-18 20:44:35 -0600

    Ultimos detaller

--- docs/js/controller/placeCtrl.js
+++ docs/js/controller/placeCtrl.js
@@ -1,6 +1,6 @@
 app.controller('placeCtrl', function ($scope, $http, $sce) {
     var url = 'https://api.foursquare.com/v2/venues/search?v=20161016&';
-    var credenciales = '&limit=1&client_id=YR1ALEYFHABZHNQMPN0IPR1GCHT0BLHH1UIAEYNINF41EIX3&client_secret=02WWTLZWCOKS4YYPZM23MX5Z14YMI5CATH0NG4G4YRF5TFI3';
+    var credenciales = '&limit=10&client_id=YR1ALEYFHABZHNQMPN0IPR1GCHT0BLHH1UIAEYNINF41EIX3&client_secret=02WWTLZWCOKS4YYPZM23MX5Z14YMI5CATH0NG4G4YRF5TFI3';
 
     var urlPhotos = 'https://api.foursquare.com/v2/venues/';
     var credencialesPhot = 'photos?v=20161016&client_id=YR1ALEYFHABZHNQMPN0IPR1GCHT0BLHH1UIAEYNINF41EIX3&client_secret=02WWTLZWCOKS4YYPZM23MX5Z14YMI5CATH0NG4G4YRF5TFI3';
@@ -12,19 +12,11 @@ app.controller('placeCtrl', function ($scope, $http, $sce) {
         $scope.results = response.data.response.venues;
         $scope.results.photo = [];
         $scope.results.googleUrl = [];
-        
-        // console.log($scope.results);
-
-
-
-        //$http.get(urlPhotos+$scope.results[0].id+"/"+credencialesPhot).then(onPhoto, onError);
-
         angular.forEach($scope.results, function(value, key) {
             $http.get(urlPhotos+value.id+"/"+credencialesPhot).then(onPhoto, onError);
             var google = 'https://www.google.com/maps/embed/v1/place?q='+ value.location.lat+','+ value.location.lng +'&key=AIzaSyAWGcEIE20vKJ3HfeZgSAiWHfsFFocOtk8';
             $scope.results.googleUrl.push({"urlGoogle": google});
         });
-        // console.log($scope.results);
     }
 
     var onError = function () {
@@ -43,9 +35,7 @@ app.controller('placeCtrl', function ($scope, $http, $sce) {
         }else{
             var url = 'https://csmmemoriahistorica.files.wordpress.com/2014/09/default-no-image.png';
             $scope.results.photo.push({"photoUrlG":url});
-            // $scope.results.photo.push({"photoUrlG": url});
         }
-         
     }
 
     $scope.trustSrc = function(src) {
@@ -60,15 +50,12 @@ app.controller('placeCtrl', function ($scope, $http, $sce) {
         return true;
     };
 
-    //   ll=25.556867399999998%2C%20-103.415072&query=coffee&intent=checkin&
-
     $scope.citySearch = function (query) {
         if (query.match(/_(\d)+$/)) {
             query = "ll=" + query;
         } else {
             query = "near=" + query;
         }
-        // console.log(query);
         $http.get(url + query + credenciales).then(onSearchData, onError);
     }
 });
